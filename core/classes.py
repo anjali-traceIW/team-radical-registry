b855871269453e6ae436b252251095b68561d220
Danny Davies <danny.davies@infinityworks.com>
2019-10-02 16:18:28 +0100

    Created Person class.

---
+++ core/classes.py
@@ -0,0 +1,13 @@
+
+
+class Person:
+
+    def __init__(self, name, team, fave_drink, personality_trait, height, dob, fave, most_likely_to):
+        self.name = name
+        self.team = team
+        self.fave_drink = fave_drink
+        self.trait = personality_trait
+        self.height = height
+        self.dob = dob 
+        self.fave = fave
+        self.most_likely_to = most_likely_to
