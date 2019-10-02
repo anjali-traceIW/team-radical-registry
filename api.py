aa7a184dcf2b5d8b8f3d306efd6855864e0dd764
Danny Davies <danny.davies@infinityworks.com>
2019-10-02 16:49:01 +0100

    Modified api.py to return single person based on ID.

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 from core.classes import Person
 import pymysql
-from param import *
+# from param import *
+from os import environ
 
 app = Flask(__name__)
 
@@ -9,10 +10,10 @@ app = Flask(__name__)
 def get_all_people_info():
     query = "SELECT * FROM tb_People"
     connection = pymysql.connect(
-        hostname,
-        username,
-        password,
-        database_name,
+        environ.get('DB_HOST'), #host
+        environ.get('DB_USER'), #username
+        environ.get('DB_PASSWORD'), #password
+        "teams", #database
         autocommit=True
     ) 
     try:
@@ -31,8 +32,24 @@ def get_all_people_info():
 
 @app.route("/person/<person_id>", methods=["GET"])
 def get_person_info(person_id):
-    # Get person info from db
-    return person
+    query = f"SELECT * FROM tb_People WHERE P_Id={person_id}"
+    connection = pymysql.connect(
+        environ.get('DB_HOST'), #host
+        environ.get('DB_USER'), #username
+        environ.get('DB_PASSWORD'), #password
+        "teams", #database
+        autocommit=True
+    ) 
+    try:
+        with connection.cursor() as db:
+            db.execute(query)
+            person = db.fetchone()
+            print(person)
+            return jsonify(person)
+    except Exception as e:
+        print(e)
+    finally:
+        connection.close()
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8081, debug=True)
